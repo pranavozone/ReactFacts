d5dc002f77b0c4b04755964f2821daed7219c782
pranavozone <pranav@ozone.one>
2023-02-20 10:15:28 +0530

    Initial commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <main className="main-frame">
+      <h1 className="main-heading">Fun Facts about React</h1>
+      <ul className="main-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K starts on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousand of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return(
+        <nav className="nav_bar">
+           <img className="nav-icon" src="images/react-icon-small.png" alt="react-logo"/> 
+           <h3 className="nav-logo-text">ReactFacts</h3>
+           <h4 className="nav-title">React Course - Project 1</h4>
+        </nav>
+    )
+}
